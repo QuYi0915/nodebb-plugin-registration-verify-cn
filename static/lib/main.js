f1e147d18df43026cb4b2ded848e8334ea1e71d9
quyi01 <quyi01@zuoyebang.com>
2020-05-15 18:25:32 +0800

    v2

--- static/lib/main.js
+++ static/lib/main.js
@@ -3,6 +3,16 @@
 
 $(function () {
     $(window).on('action:ajaxify.end', function (err,data) {
-
+        //console.log(utils.param('error'));
+        if (data.tpl_url === 'register' && utils.param('error') === 'wrong-code') {
+            require(['translator'], function (translator) {
+                translator.translate('[[verify-code:client-wrong-code]]', function (translated) {
+                    console.log('Translated string:', translated);
+                    //utils.param('error') = translated;
+                    app.alertError(translated);
+                });
+            });
+            
+        }
     });
 });
