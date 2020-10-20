8f75445c6ceb8fff186f25fd1b772d53fdac742f
QuYi <1096496326@qq.com>
2020-10-20 10:27:13 +0800

    Update main.js

--- static/lib/main.js
+++ static/lib/main.js
@@ -6,11 +6,11 @@ $(function () {
         $.get("/api/verify-code-refresh", function (res) {
             $("#verifcCode").attr("src", res.dataURL);
         });
-    }
+    };
     $(window).on('action:ajaxify.end', function (err, data) {
         if (data.tpl_url === 'register') {
             refresh();
-        }
+        };
         $("#verifcCode").on("click", refresh);
     });
 });
