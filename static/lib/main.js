a30cad1e9396d97cd1900e8030ae2688e1158708
QuYi <1096496326@qq.com>
2020-10-20 10:23:19 +0800

    Update main.js

--- static/lib/main.js
+++ static/lib/main.js
@@ -2,11 +2,15 @@
 /*global utils, app*/
 
 $(function () {
+    function refresh(){
+        $.get("/api/verify-code-refresh", function (res) {
+            $("#verifcCode").attr("src", res.dataURL);
+        });
+    }
     $(window).on('action:ajaxify.end', function (err, data) {
         if (data.tpl_url === 'register') {
-            $.get("/api/verify-code-refresh", function (res) {
-                $("#verifcCode").attr("src", res.dataURL);
-            });
+            refresh();
         }
+        $("#verifcCode").on("click", refresh);
     });
 });
