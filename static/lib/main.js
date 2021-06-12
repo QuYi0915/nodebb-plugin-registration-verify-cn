69e376ccfecbf046749c960d7509b0468d241980
QuYi <1096496326@qq.com>
2021-06-12 18:44:52 +0800

    Update main.js

--- static/lib/main.js
+++ static/lib/main.js
@@ -9,6 +9,8 @@ $(function () {
     };
     $(window).on('action:ajaxify.end', function (err, data) {
         if (data.tpl_url === 'register') {
+            $('#register').html('已备注用户名并支付30元，立即注册');
+            $('#register').click(function(){document.getElementsByTagName("html")[0].scrollTop=0});
             refresh();
         };
         $("#verifcCode").on("click", refresh);
